a424ebba89035d3762d3450f14125275e8da263f
therainmaiker <57842299+therainmaiker@users.noreply.github.com>
2021-05-31 21:28:39 +0800

    Models & Datas forlder added

---
+++ Xamarin forms khawi/Xamarin forms khawi/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Xamarin_forms_khawi.Models
+{
+    class Product
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+
+
+    }
+}
